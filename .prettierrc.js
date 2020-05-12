8a0c4f0c3e22dc5276cd5bc599c82ac45690145c
HyeIn Ju <hyein.ju92@gmail.com>
2020-05-12 13:38:51 +0900

    Add: eslint, prettier 적용

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    trailingComma: "all",
+    singleQuote: false,
+    printWidth: 100,
+    tabWidth: 4,
+    orderedImports: true,
+    bracketSpacing: true,
+};
